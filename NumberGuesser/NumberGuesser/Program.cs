f50b8f5f7883949597e4011edb8a4ebd0b6ddb72
STIVVVV <stevencanfield2000@gmail.com>
2022-04-07 00:26:37 -0400

    Player can now make a guess as to what the correct number is.

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -17,9 +17,53 @@ namespace NumberGuesser
             //Change text color
             Console.ForegroundColor = ConsoleColor.DarkGreen;  
 
-            Console.WriteLine("{0}: Version {1} BY {2}", appName, appVersion, appAuthor);
+            Console.WriteLine("{0}: Version {1} BY {2}", appName, appVersion, appAuthor + "\n");
 
             Console.ResetColor();
+
+            //Ask user's name
+            Console.WriteLine("What is your name?");
+
+            //get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets play a game...", inputName);
+
+            //Init correct number
+            int correctNumber = 7;
+
+            //Init guess var
+            int guess = 0;
+
+
+            //ask user for a number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            //while guess is not correct
+            while (guess != correctNumber)
+            {
+                string input = Console.ReadLine();
+
+                //Cast to int and put in guess
+                guess = Int32.Parse(input);
+
+                
+
+                if (guess != correctNumber)
+                {
+                    //Change text color
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    //Write out app info
+                    Console.WriteLine("Wrong number, please try again");
+
+                    //Change text color
+                    Console.ResetColor();
+                }
+
+                   
+
+            }
         }
     }
 }
