e7b197a01210bdf991a468947d8c3360846e66b8
STIVVVV <stevencanfield2000@gmail.com>
2022-04-06 23:23:33 -0400

    Now sets text color and and made three new variables

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -9,10 +9,17 @@ namespace NumberGuesser
         //Entry Point Method
         static void Main(string[] args)
         {
-            string name = "carl";
+            //Set app Vars
+            string appName = "NumberGuesser Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Brad Traversy";
 
-            //Start Here
-            Console.WriteLine("Hello " + name);
+            //Change text color
+            Console.ForegroundColor = ConsoleColor.DarkGreen;  
+
+            Console.WriteLine("{0}: Version {1} BY {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
         }
     }
 }
