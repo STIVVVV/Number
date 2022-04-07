5949e57d39443814c7a2cc70d0faa1db4036c74f
STIVVVV <stevencanfield2000@gmail.com>
2022-04-07 01:00:17 -0400

    The number is now random number and there is a catch block to make sure it is a number.

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -30,7 +30,12 @@ namespace NumberGuesser
             Console.WriteLine("Hello {0}, lets play a game...", inputName);
 
             //Init correct number
-            int correctNumber = 7;
+            //int correctNumber = 7;
+
+            //create a new random object
+            Random random = new Random();
+
+            int correctNumber = random.Next(1, 10);
 
             //Init guess var
             int guess = 0;
@@ -44,26 +49,47 @@ namespace NumberGuesser
             {
                 string input = Console.ReadLine();
 
+                //make sure its a number
+                if (!int.TryParse(input, out guess))
+                {
+                    //Change text color
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    //tell user its the wrong number
+                    Console.WriteLine("Please enter actual number");
+
+                    //Change text color
+                    Console.ResetColor();
+
+                    //keep going
+                    continue;
+                }
+
                 //Cast to int and put in guess
                 guess = Int32.Parse(input);
-
-                
 
                 if (guess != correctNumber)
                 {
                     //Change text color
                     Console.ForegroundColor = ConsoleColor.DarkRed;
 
-                    //Write out app info
+                    //tell user its the wrong number
                     Console.WriteLine("Wrong number, please try again");
 
                     //Change text color
                     Console.ResetColor();
                 }
-
-                   
-
             }
+
+            //output success message
+            //Change text color
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            //tell the user its the right number
+            Console.WriteLine("correct number");
+
+            //Change text color
+            Console.ResetColor();
         }
     }
 }
